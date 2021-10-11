e53f193d8552ce94f96247fb5e5f90d535208bde
Andrew Monteiro <andrew.agoravai@gmail.com>
2021-10-10 23:06:51 -0300

    Refactor: decentralize UI methods

--- src/com/ndrewcoding/main/Menu.java
+++ src/com/ndrewcoding/main/Menu.java
@@ -32,35 +32,7 @@ public class Menu {
     public static boolean pause = false;
 
     public void tick() {
-        if (up) {
-            up = false;
-            if (!languageIsSet) {
-                currentLanguage--;
-                if (currentLanguage < 0) {
-                    currentLanguage = (maxLanguage - 1);
-                }
-            } else {
-                currentOption--;
-                if (currentOption < 0) {
-                    currentOption = (maxOption - 1);
-                }
-            }
-            Sound.menuChange.play();
-        } else if (down) {
-            down = false;
-            if (!languageIsSet) {
-                currentLanguage++;
-                if (currentLanguage >= maxLanguage) {
-                    currentLanguage = 0;
-                }
-            } else {
-                currentOption++;
-                if (currentOption >= maxOption) {
-                    currentOption = 0;
-                }
-            }
-            Sound.menuChange.play();
-        }
+        moveThroughDisplayedMenu();
 
         if (enter) {
             enter = false;
@@ -114,6 +86,38 @@ public class Menu {
 
     }
 
+    private void moveThroughDisplayedMenu() {
+        if (up) {
+            up = false;
+            if (!languageIsSet) {
+                currentLanguage--;
+                if (currentLanguage < 0) {
+                    currentLanguage = (maxLanguage - 1);
+                }
+            } else {
+                currentOption--;
+                if (currentOption < 0) {
+                    currentOption = (maxOption - 1);
+                }
+            }
+            Sound.menuChange.play();
+        } else if (down) {
+            down = false;
+            if (!languageIsSet) {
+                currentLanguage++;
+                if (currentLanguage >= maxLanguage) {
+                    currentLanguage = 0;
+                }
+            } else {
+                currentOption++;
+                if (currentOption >= maxOption) {
+                    currentOption = 0;
+                }
+            }
+            Sound.menuChange.play();
+        }
+    }
+
     public void render(Graphics g) {
         g.setColor(new Color(0, 0, 0, 227));
         g.fillRect(0, 0, Game.WIDTH * Game.SCALE, Game.HEIGHT * Game.SCALE);
@@ -123,37 +127,14 @@ public class Menu {
 
         g.setColor(Color.white);
         g.setFont(new Font("arial", Font.PLAIN, 20));
+        
         if (!languageIsSet) {
-            Menu.drawCenteredString("Selecione o idioma:", 480, 200, g);
-            Menu.drawCenteredString("Português", 480, 250, g);
-            Menu.drawCenteredString("English", 480, 300, g);
-            Menu.drawCenteredString("Deutsch", 480, 350, g);
-
-            if (languages[currentLanguage].equals("pt-br") && !pause) {
-                g.setColor(Color.YELLOW);
-                Menu.drawCenteredString("Português", 480, 250, g);
-            } else if (languages[currentLanguage].equals("en-us")) {
-                g.setColor(Color.YELLOW);
-                Menu.drawCenteredString("English", 480, 300, g);
-            } else if (languages[currentLanguage].equals("de-de")) {
-                g.setColor(Color.YELLOW);
-                Menu.drawCenteredString("Deutsch", 480, 350, g);
-            }
+            displayLanguageMenu(g);
         } else {
             if (!pause) {
-                Menu.drawCenteredString(language.newGame, 480, 200, g);
-                g.setColor(Color.white);
-                g.setFont(new Font("century gothic", Font.BOLD, 12));
-                Menu.drawCenteredString(language.gameMadeBy, 480, 380, g);
-                Menu.drawCenteredString("Andrew Monteiro", 480, 395, g);
-                g.setColor(new Color(0, 255, 128));
-                Menu.drawCenteredString(language.specialThanks, 480, 440, g);
+                displayInitialMenu(g);
             } else {
-                Menu.drawCenteredString(language.continueGame, 480, 200, g);
-                g.setColor(Color.yellow);
-                g.setFont(new Font("impact", Font.PLAIN, 20));
-                Menu.drawCenteredString(language.currentScore, 480, 375, g);
-                Menu.drawCenteredString(Player.score + "", 480, 405, g);
+                displayPauseMenu(g);
             }
 
             g.setColor(Color.white);
@@ -161,25 +142,65 @@ public class Menu {
             Menu.drawCenteredString(language.myGithub, 480, 250, g);
             Menu.drawCenteredString(language.exit, 480, 300, g);
 
-            if (options[currentOption].equals("novo_jogo") && !pause) {
-                g.setColor(Color.YELLOW);
-                Menu.drawCenteredString(language.newGame, 480, 200, g);
-            } else if (options[currentOption].equals("novo_jogo")) {
-                g.setColor(Color.YELLOW);
-                Menu.drawCenteredString(language.continueGame, 480, 200, g);
-            } else if (options[currentOption].equals("meu_github")) {
-                g.setColor(Color.YELLOW);
-                Menu.drawCenteredString(language.myGithub, 480, 250, g);
-            } else if (options[currentOption].equals("sair")) {
-                g.setColor(Color.YELLOW);
-                Menu.drawCenteredString(language.exit, 480, 300, g);
-            }
+            highlightSelectedOption(g);
 
             g.setColor(Color.CYAN);
             g.setFont(new Font("arial", Font.ITALIC, 15));
             Menu.drawCenteredString(language.controls, 480, 335, g);
 
             g.drawImage(Game.player.spriteRight[index], ((Game.WIDTH * Game.SCALE) / 2) - 23, 128, 48, 48, null);
+        }
+    }
+
+    private void displayLanguageMenu(Graphics g) {
+        Menu.drawCenteredString("Selecione o idioma:", 480, 200, g);
+        Menu.drawCenteredString("Português", 480, 250, g);
+        Menu.drawCenteredString("English", 480, 300, g);
+        Menu.drawCenteredString("Deutsch", 480, 350, g);
+
+        if (languages[currentLanguage].equals("pt-br") && !pause) {
+            g.setColor(Color.YELLOW);
+            Menu.drawCenteredString("Português", 480, 250, g);
+        } else if (languages[currentLanguage].equals("en-us")) {
+            g.setColor(Color.YELLOW);
+            Menu.drawCenteredString("English", 480, 300, g);
+        } else if (languages[currentLanguage].equals("de-de")) {
+            g.setColor(Color.YELLOW);
+            Menu.drawCenteredString("Deutsch", 480, 350, g);
+        }
+    }
+
+    private void displayInitialMenu(Graphics g) {
+        Menu.drawCenteredString(language.newGame, 480, 200, g);
+        g.setColor(Color.white);
+        g.setFont(new Font("century gothic", Font.BOLD, 12));
+        Menu.drawCenteredString(language.gameMadeBy, 480, 380, g);
+        Menu.drawCenteredString("Andrew Monteiro", 480, 395, g);
+        g.setColor(new Color(0, 255, 128));
+        Menu.drawCenteredString(language.specialThanks, 480, 440, g);
+    }
+    
+    private void displayPauseMenu(Graphics g) {
+        Menu.drawCenteredString(language.continueGame, 480, 200, g);
+        g.setColor(Color.yellow);
+        g.setFont(new Font("impact", Font.PLAIN, 20));
+        Menu.drawCenteredString(language.currentScore, 480, 375, g);
+        Menu.drawCenteredString(Player.score + "", 480, 405, g);
+    }
+
+    private void highlightSelectedOption(Graphics g) {
+        if (options[currentOption].equals("novo_jogo") && !pause) {
+            g.setColor(Color.YELLOW);
+            Menu.drawCenteredString(language.newGame, 480, 200, g);
+        } else if (options[currentOption].equals("novo_jogo")) {
+            g.setColor(Color.YELLOW);
+            Menu.drawCenteredString(language.continueGame, 480, 200, g);
+        } else if (options[currentOption].equals("meu_github")) {
+            g.setColor(Color.YELLOW);
+            Menu.drawCenteredString(language.myGithub, 480, 250, g);
+        } else if (options[currentOption].equals("sair")) {
+            g.setColor(Color.YELLOW);
+            Menu.drawCenteredString(language.exit, 480, 300, g);
         }
     }
 
