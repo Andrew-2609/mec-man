3b5d6184aaa1387e07240b1dd4c486e69d38e786
Andrew Monteiro <andrew.agoravai@gmail.com>
2022-01-24 21:52:10 -0300

    Refactor: correct identation of Spritesheet class

--- src/com/ndrewcoding/graphics/Spritesheet.java
+++ src/com/ndrewcoding/graphics/Spritesheet.java
@@ -8,18 +8,18 @@ import javax.imageio.ImageIO;
 
 public class Spritesheet {
 
-	private BufferedImage spritesheet;
-	
-	public Spritesheet(String path)
-	{
-		try {
-			spritesheet = ImageIO.read(Objects.requireNonNull(getClass().getResource(path), "The spritesheet cannot be null!!"));
-		} catch (IOException e) {
-			System.out.println("The was a problem while reading the spritesheet. Error: " + e.getMessage());
-		}
-	}
-	
-	public BufferedImage getSprite(int x,int y,int width,int height){
-		return spritesheet.getSubimage(x, y, width, height);
-	}
+    private BufferedImage spritesheet;
+
+    public Spritesheet(String path) {
+        try {
+            spritesheet = ImageIO.read(Objects.requireNonNull(getClass().getResource(path), "The spritesheet cannot be null!!"));
+        } catch (IOException e) {
+            System.out.println("The was a problem while reading the spritesheet. Error: " + e.getMessage());
+        }
+    }
+
+    public BufferedImage getSprite(int x, int y, int width, int height) {
+        return spritesheet.getSubimage(x, y, width, height);
+    }
+
 }
